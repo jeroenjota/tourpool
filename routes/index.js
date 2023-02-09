15d19b6962f0a3cd8bdb0b528924ebde619db15d
jeroenjota <jeroen@jota.nl>
2023-02-09 14:59:37 +0100

    wip

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
   const qrySet = "SET lc_time_names = 'nl_NL'; "
   con.db.query(qrySet, (err, result) => {
     if (err) { console.log("FOUT: " + err) }
-    const tourQry = "SELECT tournaam, jaar,Date_Format(startdatum,'%W %e %M') as startdatum,	Date_Format(einddatum,'%W %e %M') as einddatum,	nummer,	Date_Format(datum,'%W %e %M') as datum,	van,	naar, 	afstand,	rittype FROM tours t JOIN etappes e ON e.tour_id = t.id"
+    const tourQry = "SELECT tournaam, jaar,Date_Format(startdatum,'%W %e %M') as startdatum,	Date_Format(einddatum,'%W %e %M') as einddatum,	nummer,	Date_Format(datum,'%a %e %M') as datum,	van,	naar, 	afstand,	rittype FROM tours t JOIN etappes e ON e.tour_id = t.id"
     const poolQry = "SELECT * FROM pools WHERE tour_id = ( SELECT id FROM tours WHERE jaar = ?) "
     const curYear = new Date().getFullYear()
     con.db.query(tourQry, curYear, (err, tourrows) => {
